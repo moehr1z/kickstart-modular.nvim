c8b4df048180d78ed3f0347d4bc256b825f63f53
moehr1z <mooehr1z@gmail.com>
2025-07-19 22:08:56 +0200

    Add leap

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -40,6 +40,7 @@ require('lazy').setup({
   require 'plugins.neo-tree',
   require 'plugins.lazygit',
   require 'plugins.vim-tmux-navigator',
+  require 'plugins.leap',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
---
+++ lua/plugins/leap.lua
@@ -0,0 +1,17 @@
+return {
+  'ggandor/leap.nvim',
+  dependencies = {
+    'tpope/vim-repeat',
+  },
+  config = function()
+    require('leap').set_default_mappings()
+    -- Exclude whitespace and the middle of alphabetic words from preview:
+    --   foobar[baaz] = quux
+    --   ^----^^^--^^-^-^--^
+    require('leap').opts.preview_filter = function(ch0, ch1, ch2)
+      return not (ch1:match '%s' or ch0:match '%a' and ch1:match '%a' and ch2:match '%a')
+    end
+    require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+    require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+  end,
+}
