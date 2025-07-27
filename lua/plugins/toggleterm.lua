88ccac8e6e2efe9421104089796d9e20cacabc50
moehr1z <mooehr1z@gmail.com>
2025-07-27 21:13:08 +0200

    toggleterm: Change keymap

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {--[[ things you want to change go here]]
-      open_mapping = [[<leader>t]],
+      open_mapping = [[<C-_>]], -- this is actually ctrl + 7 on a qwertz keyboard
     },
   },
 }
